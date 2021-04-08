04f2caf75537684abe7f835447b2c8146a5bc805
Eve <e_sotnik@bk.ru>
2021-04-08 21:47:22 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RobotControl
+{
+    // изменить код управления боевым роботом:
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ShouldFire2(true, "Enemy", 100));
+        }
+
+		static bool ShouldFire(bool enemyInFront, string enemyName, int robotHealth)
+		{
+			bool shouldFire = true;
+			if (enemyInFront == true)
+			{
+				if (enemyName == "boss")
+				{
+					if (robotHealth < 50) shouldFire = false;
+					if (robotHealth > 100) shouldFire = true;
+				}
+			}
+			else
+			{
+				return false;
+			}
+			return shouldFire;
+		}
+
+		static bool ShouldFire2(bool enemyInFront, string enemyName, int robotHealth)
+        {
+            return (enemyInFront && (enemyName == "boss" && robotHealth >= 50)) || (enemyInFront && enemyName != "boss");
+        }
+    }
+}
